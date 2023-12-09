526e0446c99f7df599d1ff9c790ba02e155e4fad
Malhar Lumbhani <malharlumbhani@Malhars-MacBook-Air.local>
2023-12-08 23:36:25 -0500

    updated temperature

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -18,7 +18,7 @@ router.get("/toronto", async (req, res, next) => {
     try {
         const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=43.7001&longitude=-79.4163&current=temperature_2m&hourly=temperature_2m");
         const data = await response.json();
-        const currentTemp = data?.hourly?.temperature_2m?.[0];
+        const currentTemp = data?.current?.temperature_2m;
 
         res.json(currentTemp);
     } catch (error) {
@@ -31,7 +31,7 @@ router.get("/jalandhar", async (req, res, next) => {
     try {
         const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=31.3256&longitude=75.5792&current=temperature_2m&hourly=temperature_2m");
         const data = await response.json();
-        const currentTemp = data?.hourly?.temperature_2m?.[0];
+        const currentTemp = data?.current?.temperature_2m;
 
         res.json(currentTemp);
     } catch (error) {
